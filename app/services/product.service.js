4236bd1684020355cdca20bd83d2df4e45015672
Josse Josse Moncheaux <lordsslypgmail.com>
2022-11-15 20:40:31 +0100

    [DC-7] ajout de pagination dans produits

--- app/services/product.service.js
+++ app/services/product.service.js
@@ -2,8 +2,26 @@ const products = require('../models/product.model')
 const Category = require("../services/category.service");
 
 const findAll = async ()  => {
- return await products.find()
-}
+     return await products.find()
+    }
+    
+
+const page = async (page)  => {
+
+     let pageEnd = page * 10
+     let pageStart = pageEnd -10
+
+     const all = await products.find()
+
+
+     let pageTotal = Math.floor(all.length /10)+1
+
+     const ChoicePage = all.slice(pageStart, pageEnd)
+
+     const infoPage={productsPage:ChoicePage,totalPages: pageTotal}
+
+      return infoPage
+     }
 
 const create = async (body,image) => {
      console.log(image);
@@ -26,10 +44,13 @@ const create = async (body,image) => {
 }
 const findOneProduct = async(req)=>{
      const callOneProduct = await products.findById(req)
+
+     return callOneProduct
+   
 }
 
 const deleteProduct = async (obj) => {
-     await products.findByIdAndDelete(obj.id);
+    return await products.findByIdAndDelete(obj.id);
 }
 
-module.exports = {findAll, create, findOneProduct, deleteProduct};
+module.exports = {findAll,page, create, findOneProduct, deleteProduct};
