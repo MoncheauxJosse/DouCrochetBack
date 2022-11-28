84dc2774ded5a1da6b2ea83048eaf83399d75e93
nuages59 <nuages59@hotmail.fr>
2022-11-28 14:15:25 +0100

    resolve conflict

--- server.js
+++ server.js
@@ -25,17 +25,17 @@ const connectionParams={
     useUnifiedTopology: true 
 }
 // Pour se connecter en local
-mongoose.connect(mongoDB).then(r => {
-    console.log('Connected to MongoDB')
-});
+// mongoose.connect(mongoDB).then(r => {
+//     console.log('Connected to MongoDB')
+// });
 
 // Pour se connecter au live
-// mongoose.connect(url, connectionParams).then(() => {
-//     console.log("Connected to MongoDB")
-// })
-// .catch( (err) => {
-//     console.error(`Error connecting to the database. n${err}`);
-// });
+mongoose.connect(url, connectionParams).then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch( (err) => {
+    console.error(`Error connecting to the database. n${err}`);
+});
 
 const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
